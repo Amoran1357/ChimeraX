0bbab4558a32618409f82b1fa09856a70aa9c79c
Amoran1357 <tgiafrican@gmail.com>
2026-06-22 19:38:17 +0300

    Add challenge function to evaluate voting results

---
+++ Debates/Challenger.ts
@@ -0,0 +1,17 @@
+export function challenge(votes:any[]) {
+
+  const buyVotes =
+    votes.filter(v => v.vote === "BUY");
+
+  const sellVotes =
+    votes.filter(v => v.vote === "SELL");
+
+  return {
+    majority: buyVotes.length > sellVotes.length
+      ? "BUY"
+      : "SELL",
+    challenge:
+      "What evidence contradicts the majority?"
+  };
+
+}
