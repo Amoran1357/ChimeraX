97b1029d5086f17fc7c2ac43523a21a48b72460b
Amoran1357 <tgiafrican@gmail.com>
2026-06-24 09:40:59 +0300

    Update Trades.ts

--- Db/Trades.ts
+++ Db/Trades.ts
@@ -9,3 +9,20 @@ export async function saveTrade(
     .insert(trade);
 
 }
+import { supabase } from "./supabase";
+
+export async function saveTrade(
+  action: string,
+  token: string,
+  amount: number
+) {
+
+  return supabase
+    .from("trades")
+    .insert({
+      action,
+      token,
+      amount
+    });
+
+}
